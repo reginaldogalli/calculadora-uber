51b465f1f59b3ee8344b6a18d2656a32490a0008
Reginaldo G Santos <reginaldo.galli@hotmail.com>
2023-02-25 18:43:36 -0300

    diversos

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+/*let valorPago = 80000;
+let valorParcela = 0;
+let qntParcelas = 0;
+let anos = 5;
+let kmMes = 5000;
+let depreciacao = 13;
+let salario = 1;
+let ganhoKm = 1;
+let ipva = 0;
+let seguro = 0;
+let valorPneu = 1500;
+let kmPneu = 40000;
+let valorOleo = 300;
+let kmOleo = 10000;
+let valorManut = 0;
+let gastosMensais = 0;
+let valorGasolina = 5;
+let consumoGasolina = 15;
+let valorEtanol = 4;
+let consumoEtanol = 10;
+let valorGnv = 4,80;
+let consumoGnv = 22;*/
+
 const calcular = document.getElementById('calcular');
 
 function calc () {
